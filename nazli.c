a0fbe81c30eebfb2070cc45e31e6c490f934dbac
Nazli Ipek Iskender <nazisken@k1m05s01.42kocaeli.com.tr>
2024-01-18 14:43:55 +0300

    ben

---
+++ nazli.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int main()
+{
+    if (1)
+    {
+        printf("%s", "clown or nazli\n");
+        printf("%s", "clown or nazli\n");
+        printf("%s", "clown or nazli\n");
+        printf("%s", "clown or nazli\n");
+        printf("%s", "clown or nazli\n");
+        printf("%s", "clown or nazli\n");
+        printf("%s", "clown or nazli\n");
+        printf("%s", "clown or nazli\n");
+        printf("%s", "clown or nazli\n");
+        printf("%s", "clown or nazli\n");
+        printf("%s", "clown or nazli\n");
+        printf("%s", "clown or nazli\n");
+        printf("%s", "clown or nazli\n");
+        printf("%s", "clown or nazli\n");
+        printf("%s", "clown or nazli\n");
+        printf("%s", "clown or nazli\n");
+        printf("%s", "clown or nazli\n");
+        printf("%s", "clown or nazli\n");
+        printf("%s", "clown or nazli\n");
+        printf("%s", "clown or nazli\n");
+        printf("%s", "clown or nazli\n");
+        printf("%s", "clown or nazli\n");
+        printf("%s", "clown or nazli\n");
+        printf("%s", "clown or nazli\n");
+        printf("%s", "clown or nazli\n");
+        printf("%s", "clown or nazli\n");
+        printf("%s", "clown or nazli\n");
+        printf("%s", "clown or nazli\n");
+        printf("%s", "clown or nazli\n");
+        printf("%s", "clown or nazli\n");
+        printf("%s", "clown or nazli\n");
+        printf("%s", "s e y 31 s e y");
+        
+    }
+}
